2eb58b57755e5e7a9d460388b9b907f39cf79e5d
Nicky <nickela125@gmail.com>
2022-02-07 15:25:09 +1300

    Added all the initial components with simple UI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class KeyboardButton extends React.Component {
+    render() {
+        return <button>{this.props.label}</button>
+    }
+}
+
+class Keyboard extends React.Component {
+    render () {
+        const keyboardLabels = [
+            ['Q','W','E','R','T','Y','U','I','O','P'],
+            ['A','S','D','F','G','H','J','K','L'],
+            ['↶','Z','X','C','V','B','N','M','↷'],
+            ['SUBMIT']
+        ];
+        return (
+            <div>
+                {
+                    keyboardLabels.map((row, index) => {
+                        return <div key={'row-' + index}>
+                            {
+                                row.map((letter) => {
+                                    return <KeyboardButton key={letter} label={letter} />
+                                })
+                            }
+                        </div>
+                    })
+                }
+                
+            </div>        
+        );
+    }
+}
+
+class Segment extends React.Component {
+    render () {
+        return <div>{this.props.letter}</div>;
+    }
+}
+
+class Wheel extends React.Component {
+    render () {
+        return (
+            <div>
+                <Segment letter={'T'} />
+                <Segment letter={'C'} />
+                <Segment letter={'O'} />
+                <Segment letter={'M'} />
+                <Segment letter={'P'} />
+                <Segment letter={'L'} />
+                <Segment letter={'E'} />
+            </div>            
+        );
+    }
+}
+
+class Game extends React.Component {
+    render () {
+        return (
+            <div>
+                <Wheel />
+                <Keyboard />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
